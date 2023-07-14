4a3eddd45f51b9d233754ccfcbf49ab33f750d1b
Ogabek <famiogabek@gmail.com>
2023-07-14 18:42:00 +0500

    finished site

---
+++ java.js
@@ -0,0 +1,9 @@
+var n2 = +prompt("Son kirit")
+var n4 = +prompt("Ikkinchi sonni kirit")
+var n8 = +prompt("Uchinchi sonni kirit")
+if (n2 < n4 && n4 > n8 ){
+    alert("o'rta qiymat" + n2)
+}
+ else if (n4 < n8 && n8 > n4 ){
+  alert("o'rta qiymat  "  + n4)  
+}
